dedbd1f06b1bd5a15d284d72c2d2a6609c1eeb75
brother1-4752 <guddlf4752@gmail.com>
2023-08-24 15:45:19 +0900

    feat: 로그인/회원가입 기본 세팅 및 스타일링

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,3 +1,59 @@
-export default function SignIn(){
-    return <div>SignIn</div>
+import useInput from '../../hooks/useInput';
+import { SignContainer } from './SignIn.styled';
+
+export default function SignIn() {
+  const [email, setEmail, onChangeEmail, validatedByEmail] = useInput('', 'email');
+  const [password, setPassword, onChangePassword, validatedByPassword] = useInput('', 'password');
+
+  const isConfirmed = validatedByEmail && validatedByPassword;
+
+  // 아래 함수에서 데이터 가져오는 작업
+  // post: auth/signin
+
+  const handleSignInSubmit = () => {};
+
+  return (
+    <SignContainer>
+      <h1 className='sign__title'>로그인</h1>
+
+      <form className='sign__form' onSubmit={handleSignInSubmit}>
+        <div className='sign__inputs'>
+          <input
+            className='sign__input'
+            data-testid='email-input'
+            type='email'
+            id='email'
+            placeholder='이메일'
+            value={email}
+            onChange={onChangeEmail}
+            required
+          />
+          {validatedByEmail || email === '' ? null : <span style={{ color: '#18c37d' }}>이메일 틀림</span>}
+          <input
+            className='sign__input'
+            data-testid='password-input'
+            type='password'
+            id='password'
+            placeholder='비밀번호'
+            value={password}
+            onChange={onChangePassword}
+            required
+          />
+          {validatedByPassword || password === '' ? null : <span style={{ color: '#18c37d' }}>패스워드 틀림</span>}
+          <button
+            className='sign__button'
+            type='submit'
+            data-testid='signin-button'
+            disabled={isConfirmed ? false : true}
+            style={{
+              backgroundColor: isConfirmed ? '#18c37d' : 'gray',
+              cursor: isConfirmed ? 'pointer' : 'not-allowed',
+            }}
+          >
+            로그인
+          </button>
+        </div>
+      </form>
+    </SignContainer>
+  );
 }
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,3 +1,57 @@
-export default function SignUp(){
-    return <div>SignUp</div>
+import useInput from '../../hooks/useInput';
+import { SignContainer } from '../SignIn/SignIn.styled';
+
+export default function SignUp() {
+  const [email, setEmail, onChangeEmail, validatedByEmail] = useInput('', 'email');
+  const [password, setPassword, onChangePassword, validatedByPassword] = useInput('', 'password');
+
+  const isConfirmed = validatedByEmail && validatedByPassword;
+
+  // 아래 함수에서 데이터 가져오는 작업
+  // post: auth/signup
+
+  const handleSignUpSubmit = () => {};
+
+  return (
+    <SignContainer>
+      <h1 className='sign__title'>회원가입</h1>
+
+      <form className='sign__form' onSubmit={handleSignUpSubmit}>
+        <div className='sign__inputs'>
+          <input
+            className='sign__input'
+            data-testid='email-input'
+            type='email'
+            id='email'
+            placeholder='이메일'
+            value={email}
+            onChange={onChangeEmail}
+            required
+          />
+          <input
+            className='sign__input'
+            data-testid='password-input'
+            type='password'
+            id='password'
+            placeholder='비밀번호'
+            value={password}
+            onChange={onChangePassword}
+            required
+          />
+          <button
+            className='sign__button'
+            type='submit'
+            data-testid='signup-button'
+            disabled={isConfirmed ? false : true}
+            style={{
+              backgroundColor: isConfirmed ? '#18c37d' : 'gray',
+              cursor: isConfirmed ? 'pointer' : 'not-allowed',
+            }}
+          >
+            회원가입
+          </button>
+        </div>
+      </form>
+    </SignContainer>
+  );
 }
