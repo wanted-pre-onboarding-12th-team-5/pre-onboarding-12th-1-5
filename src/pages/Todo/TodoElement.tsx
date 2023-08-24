debadb3ebf8bd019d3e3067d9addde81a0f93446
chichoon <chichoon.choi@gmail.com>
2023-08-24 20:21:50 +0900

    fix: todoDeleteHandler 를 handleClickDelete로 이동

--- src/pages/Todo/TodoElement.tsx
+++ src/pages/Todo/TodoElement.tsx
@@ -1,18 +1,19 @@
 import { ChangeEvent, Dispatch, SetStateAction, useCallback, useState } from 'react';
 
-import { deleteTodo, updateTodo } from 'services/todoInstance';
-import { Todo } from './todo.d';
+import { deleteTodo, updateTodo } from '../../services/todoInstance';
+import { TodoType } from './todo.d';
 
 import { StyledTodoElement } from './TodoElement.styled';
 
 interface Props {
-  todo: Todo;
-  setTodoList: Dispatch<SetStateAction<Todo[]>>;
+  todo: TodoType;
+  setTodoList: Dispatch<SetStateAction<TodoType[]>>;
 }
 
 export const TodoElement = ({ todo, setTodoList }: Props) => {
   const [isEditing, setIsEditing] = useState(false);
   const [newTodoTitle, setNewTodoTitle] = useState(todo.todo);
+  const [clickEnabled, setClickEnabled] = useState(true);
 
   const handleChangeCheckbox = useCallback(() => {
     updateTodo(todo.id, todo.todo, !todo.isCompleted).then((data) => {
@@ -40,14 +41,18 @@ export const TodoElement = ({ todo, setTodoList }: Props) => {
     setNewTodoTitle(todo.todo);
   }, [todo]);
 
-  const handleClickDelete = useCallback(() => {
-    // eslint-disable-next-line no-restricted-globals
-    if (confirm('정말 삭제하시겠습니까?')) {
-      deleteTodo(todo.id).then(() => {
-        setTodoList((prev) => prev.filter((element) => element.id !== todo.id));
-      });
+  const handleClickDelete = async (todoId: number) => {
+    if (clickEnabled) {
+      const confirmDelete = window.confirm('정말 삭제하시겠습니까?');
+      if (confirmDelete) {
+        setClickEnabled(false);
+        await deleteTodo(todoId).then(() => {
+          setTodoList((prev) => prev.filter((element) => element.id !== todoId));
+          setClickEnabled(true);
+        });
+      }
     }
-  }, [setTodoList, todo]);
+  };
 
   if (isEditing)
     return (
@@ -85,7 +90,12 @@ export const TodoElement = ({ todo, setTodoList }: Props) => {
       <button data-testid='modify-button' onClick={handleClickModify} className='todo-button'>
         수정
       </button>
-      <button data-testid='delete-button' onClick={handleClickDelete} className='todo-button'>
+      <button
+        data-testid='delete-button'
+        onClick={() => handleClickDelete(todo.id)}
+        className='todo-button'
+        disabled={!clickEnabled}
+      >
         삭제
       </button>
     </StyledTodoElement>
