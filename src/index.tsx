c78f0bf249f12d182a325410d95457132048cc91
jihye yu <dbwlgp1yng@gmail.com>
2023-08-24 21:18:14 +0900

    feat: 토큰 유무에 따른 라우팅 처리

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <Router>
+      <GlobalStyle />
+      <App />
+    </Router>
   </React.StrictMode>
 );
