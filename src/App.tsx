753479dff9446b3764882de2ccac506919de6108
brother1-4752 <guddlf4752@gmail.com>
2023-08-24 13:29:27 +0900

    feat: 라우팅 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home, SignIn, SignUp, Todo } from './pages';
+
+//errorBoundary
 function App() {
-  return <div>APP</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/todo' element={<Todo />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from './Home/index'
+export { default as SignIn } from './SignIn/index'
+export { default as SignUp } from './SignUp/index'
+export { default as Todo } from './Todo/index'
