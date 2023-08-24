a5b1349fc57835e90ffd34399a7f9bcb8d1af53c
chichoon <chichoon.choi@gmail.com>
2023-08-24 20:19:07 +0900

    feat: todo 래퍼 컴포넌트 수정

--- src/pages/Todo/Todo.tsx
+++ src/pages/Todo/Todo.tsx
@@ -1,22 +1,35 @@
-import TodoList from "./TodoList";
-import {useState} from "react";
+import { useState } from 'react';
 
-export default function Todo(){
-    const [clickEnabled, setClickEnabled] = useState(true);
-    const todoDeleteHandler = async (todoId : number) => {
-        if (clickEnabled){
-            const confirmDelete = window.confirm("정말 삭제하시겠습니까?");
-            if (confirmDelete){
-                setClickEnabled(false);
-                //delete fetch
-                setClickEnabled(true);
-            }
-        }
+import { TodoType } from './todo.d';
+import { deleteTodo } from '../../services/todoInstance';
+import { TodoElement } from './TodoElement';
+
+import StyledTodoContainer from './Todo.styled';
+
+export default function TodoPage() {
+  const [clickEnabled, setClickEnabled] = useState(true);
+  const [todoList, setTodoList] = useState<TodoType[]>([]);
+
+  const todoDeleteHandler = async (todoId: number) => {
+    if (clickEnabled) {
+      const confirmDelete = window.confirm('정말 삭제하시겠습니까?');
+      if (confirmDelete) {
+        setClickEnabled(false);
+        await deleteTodo(todoId).then(() => {
+          setTodoList((prev) => prev.filter((element) => element.id !== todoId));
+          setClickEnabled(true);
+        });
+      }
     }
+  };
 
-    return (
-        <TodoList
-            todoDeleteHandler={todoDeleteHandler}
-        />
-    )
+  return (
+    <StyledTodoContainer>
+      <ul className='todo-list'>
+        {todoList.map((todo) => (
+          <TodoElement key={todo.id} todo={todo} setTodoList={setTodoList} onDelete={todoDeleteHandler} />
+        ))}
+      </ul>
+    </StyledTodoContainer>
+  );
 }
