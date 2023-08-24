abc3cfff64eef59b86e015f82578c0677ff62f98
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-08-24 17:19:10 +0900

    feat: add todoDeleteHandler, TodoList component

--- src/pages/Todo/Todo.tsx
+++ src/pages/Todo/Todo.tsx
@@ -1,3 +1,22 @@
+import TodoList from "./TodoList";
+import {useState} from "react";
+
 export default function Todo(){
-    return <div>Todo</div>
+    const [clickEnabled, setClickEnabled] = useState(true);
+    const todoDeleteHandler = async (todoId : number) => {
+        if (clickEnabled){
+            const confirmDelete = window.confirm("정말 삭제하시겠습니까?");
+            if (confirmDelete){
+                setClickEnabled(false);
+                //delete fetch
+                setClickEnabled(true);
+            }
+        }
+    }
+
+    return (
+        <TodoList
+            todoDeleteHandler={todoDeleteHandler}
+        />
+    )
 }
