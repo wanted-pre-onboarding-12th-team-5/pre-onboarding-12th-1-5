3d820fa747dd3239b15eb068da4203d2eb319500
brother1-4752 <guddlf4752@gmail.com>
2023-08-24 14:51:58 +0900

    chore: 불명확한 네이밍 수정
    - 각 페이지의 index.tsx를 [페이지이름].tsx으로 변경

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
-export { default as Home } from './Home/index'
-export { default as SignIn } from './SignIn/index'
-export { default as SignUp } from './SignUp/index'
-export { default as Todo } from './Todo/index'
+export { default as Home } from './Home/Home';
+export { default as SignIn } from './SignIn/SignIn';
+export { default as SignUp } from './SignUp/SignUp';
+export { default as Todo } from './Todo/Todo';
