cdcacb0940ef4a5d86620e3081715c6533e42a03
brother1-4752 <guddlf4752@gmail.com>
2023-08-24 15:53:18 +0900

    refactor: 홈 화면 link 중복 코드 리팩토링

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,18 +4,17 @@ import HomeContainer from './Home.styled';
 function Home() {
   return (
     <HomeContainer>
-      <h1 className="home__title">
+      <h1 className='home__title'>
         원티드 프리온보딩
         <br /> 사전과제
       </h1>
 
-      <div className="home__links">
-        <Link className="home__link" to="signin">
-          로그인
-        </Link>
-        <Link className="home__link" to="signup">
-          회원가입
-        </Link>
+      <div className='home__links'>
+        {['signin', 'signup'].map((to) => (
+          <Link className='home__link' to={`${to}`}>
+            {to === 'signin' ? '로그인' : '회원가입'}
+          </Link>
+        ))}
       </div>
     </HomeContainer>
   );
