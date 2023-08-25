63cd415b65347f9b88fb6631c6d0e36d4e0e3ec7
brother1-4752 <guddlf4752@gmail.com>
2023-08-25 18:45:50 +0900

    fix: todoinstance 토큰키 일치

--- src/pages/Todo/Todo.tsx
+++ src/pages/Todo/Todo.tsx
@@ -1,29 +1,27 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
-import {TodoType} from './todo.d';
-import {TodoElement} from './TodoElement';
+import { TodoType } from './todo.d';
+import { TodoElement } from './TodoElement';
 
 import StyledTodoContainer from './Todo.styled';
-import {AddTodoForm} from './AddTodoForm';
-import {getTodos} from "../../services/todoInstance";
+import { AddTodoForm } from './AddTodoForm';
+import { getTodos } from '../../services/todoInstance';
 
 export default function TodoPage() {
   const [todoList, setTodoList] = useState<TodoType[]>([]);
 
   useEffect(() => {
-    /* 현재 Token값이 없어 무한 루프로 인한 주석처리
-    getTodos().then((res) => {ㅌ
-      setTodoList(res)
+    getTodos().then((res) => {
+      setTodoList(res);
     });
-     */
   }, []);
 
   return (
     <StyledTodoContainer>
-      <AddTodoForm setTodoList={setTodoList}/>
+      <AddTodoForm setTodoList={setTodoList} />
       <ul className='todo-list'>
         {todoList.map((todo) => (
-          <TodoElement key={todo.id} todo={todo} setTodoList={setTodoList}/>
+          <TodoElement key={todo.id} todo={todo} setTodoList={setTodoList} />
         ))}
       </ul>
     </StyledTodoContainer>
