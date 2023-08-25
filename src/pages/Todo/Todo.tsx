7ee05ec5e17b29d783e9934fd74d6d332412d049
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-08-25 13:33:29 +0900

    feat: add GetTodo 현재 Token 값을 얻는게 불가능해 주석처리

--- src/pages/Todo/Todo.tsx
+++ src/pages/Todo/Todo.tsx
@@ -1,20 +1,29 @@
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 
-import { TodoType } from './todo.d';
-import { TodoElement } from './TodoElement';
+import {TodoType} from './todo.d';
+import {TodoElement} from './TodoElement';
 
 import StyledTodoContainer from './Todo.styled';
-import { AddTodoForm } from './AddTodoForm';
+import {AddTodoForm} from './AddTodoForm';
+import {getTodos} from "../../services/todoInstance";
 
 export default function TodoPage() {
   const [todoList, setTodoList] = useState<TodoType[]>([]);
 
+  useEffect(() => {
+    /* 현재 Token값이 없어 무한 루프로 인한 주석처리
+    getTodos().then((res) => {ㅌ
+      setTodoList(res)
+    });
+     */
+  }, []);
+
   return (
     <StyledTodoContainer>
-      <AddTodoForm setTodoList={setTodoList} />
+      <AddTodoForm setTodoList={setTodoList}/>
       <ul className='todo-list'>
         {todoList.map((todo) => (
-          <TodoElement key={todo.id} todo={todo} setTodoList={setTodoList} />
+          <TodoElement key={todo.id} todo={todo} setTodoList={setTodoList}/>
         ))}
       </ul>
     </StyledTodoContainer>
